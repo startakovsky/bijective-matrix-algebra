124db8112e2e6c5c19eef04d197f2c019844f311
Steven <startakovsky@gmail.com>
2012-10-23 16:43:22 -0700

    Update stirling_matrices.py

--- stirling_matrices.py
+++ stirling_matrices.py
@@ -6,7 +6,7 @@ It generates the standard combinatorial interpretations of Stirling Matrices and
 
 AUTHORS:
 
-- Steven Tartakovsky (2012): initial version yyyy
+- Steven Tartakovsky (2012): initial version
 
 """
 #*****************************************************************************
