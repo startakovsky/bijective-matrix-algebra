8a23dd201cf289781d6438752208a478b9792cf1
startakovsky <startakovsky@gmail.com>
2012-10-23 13:34:04 -0700

    added documentation and minor fix to stirling

--- stirling_matrices.py
+++ stirling_matrices.py
@@ -1,11 +1,36 @@
+r"""
+
+This file contains the methods used in the Bijective Matrix Algebra package.
+
+It generates the standard combinatorial interpretations of Stirling Matrices and their product.
+
+AUTHORS:
+
+- Steven Tartakovsky (2012): initial version
+
+"""
+#*****************************************************************************
+#       Copyright (C) 2012 Steven Tartakovsky <startakovsky@gmail.com>, 
+#
+#  Distributed under the terms of the GNU General Public License (GPL)
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    General Public License for more details.
+#
+#  The full text of the GPL is available at:
+#
+#                  http://www.gnu.org/licenses/
+#*****************************************************************************
+
+from sage.matrix.all import *
+from sage.combinat.permutation import *
+from sage.combinat.set_partition import *
 from sage.bijectivematrixalgebra.combinatorial_objects import CombinatorialObject
 from sage.bijectivematrixalgebra.combinatorial_scalars import CombinatorialScalar
 from sage.bijectivematrixalgebra.combinatorial_scalar_rings_and_elements import CombinatorialScalarWrapper
 from sage.bijectivematrixalgebra.combinatorial_scalar_rings_and_elements import CombinatorialScalarRing
-#from sage.matrix.all import *
-#from sage.combinat.permutation import *
-#from sage.combinat.set_partition import *
-from sage.all import *
 
 
 PermutationOptions(display = 'cycle')
@@ -50,8 +75,9 @@ def matrix_multiply(mat1,mat2):
     Only works for square matrices.
     """
     mat_space = mat1.matrix_space()
+    dim = mat_space.nrows()
     l = list()
-    for row in range(n):
+    for row in range(dim):
         l.append(_product_row(mat1,mat2,row))
     return mat_space(l)
 
@@ -70,7 +96,7 @@ def Stirling2Matrix(dim):
     """
     Returns Stirling2 Matrix whose entries are Combinatorial Scalars of set partitions.
     """ 
-    mat_space = MatrixSpace(CombinatorialScalarRing(),n)
+    mat_space = MatrixSpace(CombinatorialScalarRing(),dim)
     prnt = mat_space.base_ring()
     l = list()
     for row in range(dim):
