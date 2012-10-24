967b358cb26ed384f087f5cadd258ab7f5d3c2b8
startakovsky <startakovsky@gmail.com>
2012-10-24 14:22:13 -0700

    added bma.involution_dict and bma.print_involution_dict methods

--- main.py
+++ main.py
@@ -20,7 +20,13 @@ AUTHORS:
 #
 #                  http://www.gnu.org/licenses/
 #*****************************************************************************
-#this was a tip from website... minimal importing
+
+from sage.matrix.all import *
+from sage.rings.rational_field import RationalField
+from sage.sets.finite_set_maps import FiniteSetMaps
+from sage.sets.set import Set
+from sage.bijectivematrixalgebra.stirling_matrices import matrix_generating_function
+
 
 def is_bijection(f):
 	"""
@@ -67,3 +73,26 @@ def is_involution(f):
 			if f(f(i)) != i:
 				return False
 		return True
+
+def involution_dict(mat):
+    if matrix_generating_function(mat)!=MatrixSpace(RationalField(),mat.nrows(),mat.ncols()).identity_matrix():
+        print "ERROR: Input needs to be equal to the identity."
+    else:
+        f = dict()
+        for x in range(mat.nrows()):
+            for y in range(mat.ncols()):
+                if x <> y:
+                    f[(x,y)] = mat[x,y].value.create_involution()
+                else:
+                    t = mat[x,y].value
+                    for i in t:
+                        _M = FiniteSetMaps(t)
+                        f[(x,y)] = _M.from_dict({i:i})
+        return f
+        
+def print_involution_dict(f):
+    for key in sorted(f):
+        print "---------------------------------------"
+        print "row " + str(key[0]) + ", column " + str(key[1]) + ": " + str(len(f[key].domain())) + " elements."
+        for elm in f[key].domain():
+            print str(elm) + " --> " + str(f[key](elm))
