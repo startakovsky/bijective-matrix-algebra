c5dd58ab164ff3f671276a1368ad425ae16123c8
Steven <startakovsky@gmail.com>
2012-10-29 00:19:16 -0700

    Update all.py

--- all.py
+++ all.py
@@ -4,4 +4,4 @@ from sage.bijectivematrixalgebra.combinatorial_scalar_rings_and_elements import 
 from sage.bijectivematrixalgebra.combinatorial_scalar_rings_and_elements import CombinatorialScalarRing
 import sage.bijectivematrixalgebra.main as bma
 from sage.bijectivematrixalgebra.stirling_matrices import *
-from sage.bijectivematrixalgebra.testScript import *
+#from sage.bijectivematrixalgebra.testScript import *
